3f5ccde0948e3f8a8f1f2c9d7f4c81c991e033af
Tero Parviainen <tero@teropa.info>
2014-12-27 10:49:14 +0100

    The $controller provider

--- test/angular_public_spec.js
+++ test/angular_public_spec.js
@@ -33,4 +33,10 @@ describe('angularPublic', function() {
     expect(injector.has('$compile')).toBe(true);
   });
 
+  it('sets up $controller', function() {
+    publishExternalAPI();
+    var injector = createInjector(['ng']);
+    expect(injector.has('$controller')).toBe(true);
+  });
+  
 });
